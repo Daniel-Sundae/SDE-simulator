0bf3134bf47700799d6131d31fd257c289bac707
Daniel Sanaee <sanaeedaniel@gmail.com>
2025-07-18 21:15:43 +0200

    Move implementation to headerfile

--- src/View/Outputs/OutputDispatcher.cpp
+++ src/View/Outputs/OutputDispatcher.cpp
@@ -1,7 +1,4 @@
-#include "PathChart.hpp"
-#include "DistributionChart.hpp"
 #include "OutputDispatcher.hpp"
-#include "StatusManager.hpp"
 #include <QtWidgets/QGraphicsLayout>
 
 OutputDispatcher::OutputDispatcher(QWidget *parent)
@@ -11,97 +8,18 @@ OutputDispatcher::OutputDispatcher(QWidget *parent)
     , m_distributionChartView(new QChartView(this))
 {
     layout = new QVBoxLayout(this);
-    layout->addWidget(m_statusManager);
     // Wrapper is only for aligning the Status Manager with the charts
     QVBoxLayout* groupBoxWrapperLayout = new QVBoxLayout();
     groupBoxWrapperLayout->setContentsMargins(8, 0, 8, 0);
     groupBoxWrapperLayout->addWidget(m_statusManager);
     layout->addLayout(groupBoxWrapperLayout);
+
     auto* pathChart = new PathChart();
     m_pathChartView->setChart(pathChart);
     layout->addWidget(m_pathChartView);
+
     auto* distributionChart = new DistributionChart();
     m_distributionChartView->setChart(distributionChart);
     layout->addWidget(m_distributionChartView);
     setLayout(layout);
-}
-
-////////////////////////////////////////
-//// STATUS MANAGER
-////////////////////////////////////////
-
-void OutputDispatcher::setQueryInfo(const PathQuery& pQuery) const{
-    m_statusManager->setQueryInfo(pQuery);
-}
-
-void OutputDispatcher::setStatus(const StatusSignal signal) const{
-    m_statusManager->setStatus(signal);
-}
-
-void OutputDispatcher::clearStatus() const{
-    m_statusManager->clearStatus();
-}
-
-////////////////////////////////////////
-//// PATH CHART
-////////////////////////////////////////
-
-void OutputDispatcher::updatePathChartTitle(bool allPathsPlotted) const{
-    getPathChart()->updateTitle(allPathsPlotted);
-}
-
-void OutputDispatcher::plotPathChartDriftData(const Path& driftLine) const{
-    getPathChart()->plotDriftCurve(driftLine);
-}
-
-void OutputDispatcher::plotPath(const Path& path) const{
-    getPathChart()->plotPath(path);
-}
-
-void OutputDispatcher::clearPathChart(bool clearDrift){
-    getPathChart()->clearPathChart(clearDrift);
-}
-
-void OutputDispatcher::setPathChartMaxTime(const Time time){
-    getPathChart()->setMaxTime(time);
-}
-
-////////////////////////////////////////
-//// DISTRIBUTION CHART
-////////////////////////////////////////
-
-void OutputDispatcher::updateDistributionChartTitle(const ProcessType type) const{
-    getDistributionChart()->updateTitle(type);
-}
-
-void OutputDispatcher::plotDistribution(const Distribution& distribution) const{
-    getDistributionChart()->plotDistribution(distribution);
-}
-
-void OutputDispatcher::updateDistributionChartPDF(const PDFData& pdfData) const{
-    getDistributionChart()->updateDistributionChartPDF(pdfData);
-}
-
-void OutputDispatcher::setDistributionChartSupport(const Range pdfDomain) const{
-    getDistributionChart()->setDistributionChartSupport(pdfDomain);
-}
-
-void OutputDispatcher::updateDistributionChartEV(const State EV) const{
-    getDistributionChart()->plotExpValLine(EV);
-}
-
-void OutputDispatcher::clearDistributionChart(){
-    getDistributionChart()->clearDistributionChart();
-}
-
-////////////////////////////////////////
-//// ACCESSORS
-////////////////////////////////////////
-
-PathChart* OutputDispatcher::getPathChart() const{
-    return static_cast<PathChart*>(m_pathChartView->chart());
-}
-
-DistributionChart* OutputDispatcher::getDistributionChart() const{
-    return static_cast<DistributionChart*>(m_distributionChartView->chart());
 }
--- src/View/Outputs/OutputDispatcher.hpp
+++ src/View/Outputs/OutputDispatcher.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include "Types.hpp"
+#include "PathChart.hpp"
+#include "DistributionChart.hpp"
+#include "StatusManager.hpp"
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QBoxLayout>
 #include <QtCharts/QChartView>
 #include <QtCharts/QChart>
 
 struct PathQuery;
-class PathChart;
-class DistributionChart;
-class StatusManager; // Forward declaration
 
 class OutputDispatcher : public QWidget{
     Q_OBJECT
@@ -18,28 +18,60 @@ public:
     explicit OutputDispatcher(QWidget* parent = nullptr);
 
     // StatusManager
-    void setQueryInfo(const PathQuery &pQuery) const;
-    void setStatus(const StatusSignal signal) const;
-    void clearStatus() const;
+    void setQueryInfo(const PathQuery& pQuery) const{
+        m_statusManager->setQueryInfo(pQuery);
+    }
+    void setStatus(const StatusSignal signal) const{
+        m_statusManager->setStatus(signal);
+    }
+    void clearStatus() const{
+        m_statusManager->clearStatus();
+    }
 
     // PathChart
-    void plotPath(const Path& path) const;
-    void updatePathChartTitle(bool allPathsPlotted) const;
-    void plotPathChartDriftData(const Path &driftLine) const;
-    void clearPathChart(bool clearDrift = true);
-    void setPathChartMaxTime(const Time time);
+    void plotPath(const Path& path) const{
+        getPathChart()->plotPath(path);
+    }
+    void updatePathChartTitle(bool allPathsPlotted) const{
+        getPathChart()->updateTitle(allPathsPlotted);
+    }
+    void plotPathChartDriftData(const Path &driftLine) const{
+        getPathChart()->plotDriftCurve(driftLine);
+    }
+    void clearPathChart(bool clearDrift = true){
+        getPathChart()->clearPathChart(clearDrift);
+    }
+    void setPathChartMaxTime(const Time time){
+        getPathChart()->setMaxTime(time);
+    }
 
     // DistributionChart
-    void updateDistributionChartTitle(const ProcessType t) const;
-    void plotDistribution(const Distribution& distribution) const;
-    void updateDistributionChartPDF(const PDFData& pdfData) const;
-    void setDistributionChartSupport(const Range pdfDomain) const;
-    void updateDistributionChartEV(const State EV) const;
-    void clearDistributionChart();
+    void updateDistributionChartTitle(const ProcessType t) const {
+        getDistributionChart()->updateTitle(t);
+    }
+    void plotDistribution(const Distribution& distribution) const {
+        getDistributionChart()->plotDistribution(distribution);
+    }
+    void updateDistributionChartPDF(const PDFData& pdfData) const {
+        getDistributionChart()->updateDistributionChartPDF(pdfData);
+    }
+    void setDistributionChartSupport(const Range pdfDomain) const {
+        getDistributionChart()->setDistributionChartSupport(pdfDomain);
+    }
+    void updateDistributionChartEV(const State EV) const {
+        getDistributionChart()->plotExpValLine(EV);
+    }
+    void clearDistributionChart() {
+        getDistributionChart()->clearDistributionChart();
+    }
 
 private:
-    PathChart* getPathChart() const;
-    DistributionChart* getDistributionChart() const;
+    PathChart* getPathChart() const {
+        return static_cast<PathChart*>(m_pathChartView->chart());
+    }
+    DistributionChart* getDistributionChart() const {
+        return static_cast<DistributionChart*>(m_distributionChartView->chart());
+    }
 private:
     QVBoxLayout* layout;
     StatusManager* m_statusManager;
